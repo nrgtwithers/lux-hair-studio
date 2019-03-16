e9e2501b79f3c2584875c880e2d0207f32af6e81
Nina Withers <ninarosewithers@gmail.com>
2019-03-15 20:49:33 -0400

    more changes to about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -16,16 +16,13 @@ class About extends Component {
                 <br/>
                 <h4>MEET THE OWNERS</h4>
                 <div className='row'>
-                <div className="col-md-1"></div>
                 <div className="col-md-3"><img src="https://github.com/nrgtwithers/lux-hair-studio/blob/master/Christine-B.png?raw=true" className='right' /></div>
-                <div className="col-md-6"><strong>Christine Baine</strong> Lemon drops sugar plum jelly carrot cake sweet roll tootsie roll macaroon dragée. Toffee tart bear claw gingerbread lemon drops. Icing apple pie marshmallow jelly gummi bears. Gummies halvah brownie gingerbread. Cotton candy caramels apple pie marzipan pie jelly beans. Gummies powder oat cake apple pie tart. Jujubes sesame snaps brownie chocolate bar. Cheesecake gummies icing tiramisu carrot cake apple pie cotton candy tart. Sugar plum soufflé chupa chups. Dragée apple pie tiramisu cupcake chocolate bar.</div>
+                <div className="col-md-9"><strong>Christine Baine</strong> Lemon drops sugar plum jelly carrot cake sweet roll tootsie roll macaroon dragée. Toffee tart bear claw gingerbread lemon drops. Icing apple pie marshmallow jelly gummi bears. Gummies halvah brownie gingerbread. Cotton candy caramels apple pie marzipan pie jelly beans. Gummies powder oat cake apple pie tart. Jujubes sesame snaps brownie chocolate bar. Cheesecake gummies icing tiramisu carrot cake apple pie cotton candy tart. Sugar plum soufflé chupa chups. Dragée apple pie tiramisu cupcake chocolate bar.</div>
                 <div className="col-md-1"></div>
                 </div>
                 <div className='row'>
-                <div className="col-md-2"></div>
-                <div className="col-md-6"><p className="right"><strong>Crystal Schemansky</strong> Lemon drops sugar plum jelly carrot cake sweet roll tootsie roll macaroon dragée. Toffee tart bear claw gingerbread lemon drops. Icing apple pie marshmallow jelly gummi bears. Gummies halvah brownie gingerbread. Cotton candy caramels apple pie marzipan pie jelly beans. Gummies powder oat cake apple pie tart. Jujubes sesame snaps brownie chocolate bar. Cheesecake gummies icing tiramisu carrot cake apple pie cotton candy tart. Sugar plum soufflé chupa chups. Dragée apple pie tiramisu cupcake chocolate bar.</p></div>
+                <div className="col-md-9"><p className="right"><strong>Crystal Schemansky</strong> Lemon drops sugar plum jelly carrot cake sweet roll tootsie roll macaroon dragée. Toffee tart bear claw gingerbread lemon drops. Icing apple pie marshmallow jelly gummi bears. Gummies halvah brownie gingerbread. Cotton candy caramels apple pie marzipan pie jelly beans. Gummies powder oat cake apple pie tart. Jujubes sesame snaps brownie chocolate bar. Cheesecake gummies icing tiramisu carrot cake apple pie cotton candy tart. Sugar plum soufflé chupa chups. Dragée apple pie tiramisu cupcake chocolate bar.</p></div>
                 <div className="col-md-3"><img src="https://github.com/nrgtwithers/lux-hair-studio/blob/master/Crystal-S.png?raw=true" className="left" /></div>
-                <div className="col-md-1"></div>
                 </div>
             </div>              
                 )

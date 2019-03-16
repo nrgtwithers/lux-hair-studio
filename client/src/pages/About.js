0a02f2f56df786fd8b124d36dd4493ccc3e5f9df
Nina Withers <ninarosewithers@gmail.com>
2019-03-16 10:05:12 -0400

    adding services.json

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -17,7 +17,7 @@ class About extends Component {
                 <h4>MEET THE OWNERS</h4>
                 <div className='row'>
                 <div className="col-md-3"><img src="https://github.com/nrgtwithers/lux-hair-studio/blob/master/Christine-B.png?raw=true" className='right' /></div>
-                <div className="col-md-9"><strong>Christine Baine</strong> Lemon drops sugar plum jelly carrot cake sweet roll tootsie roll macaroon dragée. Toffee tart bear claw gingerbread lemon drops. Icing apple pie marshmallow jelly gummi bears. Gummies halvah brownie gingerbread. Cotton candy caramels apple pie marzipan pie jelly beans. Gummies powder oat cake apple pie tart. Jujubes sesame snaps brownie chocolate bar. Cheesecake gummies icing tiramisu carrot cake apple pie cotton candy tart. Sugar plum soufflé chupa chups. Dragée apple pie tiramisu cupcake chocolate bar.</div>
+                <div className="col-md-9"><p><strong>Christine Baine</strong> Lemon drops sugar plum jelly carrot cake sweet roll tootsie roll macaroon dragée. Toffee tart bear claw gingerbread lemon drops. Icing apple pie marshmallow jelly gummi bears. Gummies halvah brownie gingerbread. Cotton candy caramels apple pie marzipan pie jelly beans. Gummies powder oat cake apple pie tart. Jujubes sesame snaps brownie chocolate bar. Cheesecake gummies icing tiramisu carrot cake apple pie cotton candy tart. Sugar plum soufflé chupa chups. Dragée apple pie tiramisu cupcake chocolate bar.</p></div>
                 <div className="col-md-1"></div>
                 </div>
                 <div className='row'>
